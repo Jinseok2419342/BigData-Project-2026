bd2a76658cc623aadb1913df88c2748728457067
6-112-01 <afsesfs@example.com>
2026-03-27 11:57:49 +0900

    2026_03_27

---
+++ 04_week/my_app/pages/4_++++_추가_데모_++++.py
@@ -0,0 +1,14 @@
+# 4_++++_추가_데모_++++.py
+import streamlit as st
+
+st.title("추가 벌룬 👋")
+st.markdown(
+    """ 
+    수업자료_3주차_업데이트-20260327
+    ㄴ 3교시_레이아웃_EDA대시보드.pdf
+    ㄴ 10 페이지
+    """
+)
+
+if st.button("풍선 소환"):
+    st.balloons()
